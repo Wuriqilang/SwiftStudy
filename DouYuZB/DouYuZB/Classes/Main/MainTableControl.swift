0440e87a3b7e86444c239370328f4e283cdbc867
Wuriqilang <543677991@qq.com>
2018-12-23 22:17:26 +0800

    Simple

---
+++ DouYuZB/DouYuZB/Classes/Main/MainTableControl.swift
@@ -0,0 +1,19 @@
+//
+//  MainTableControl.swift
+//  DouYuZB
+//
+//  Created by 乌日其浪 on 2018/9/9.
+//  Copyright © 2018年 乌日其浪. All rights reserved.
+//
+
+import UIKit
+
+class MainTableControl: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+  
+
+}
